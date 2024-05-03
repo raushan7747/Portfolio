f85bce2dc318a797ce4a31e682b5a155d1faa5b3
Raushan_kumar <raushanmandallkr@gmail.com>
2024-05-02 21:47:31 -0700

    responsive font-sizes and main font-size

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const menu_bar = document.getElementById('mbar-list');
 menu_bar.addEventListener('click', function () {
   this.classList.toggle('mbtn');
 });
+
+const navBar = document.getElementById('mbar-list');
+navBar.addEventListener('click', function () {
+  const navBarClass = document.getElementById('navBar')
+  navBarClass.classList.toggle('navToggle');
+});
