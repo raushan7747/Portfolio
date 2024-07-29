1a2e7228fa1787b996a931af3dacf982a74d6411
Raushan_kumar <raushanmandallkr@gmail.com>
2024-07-29 10:12:10 -0600

    added symbols and finishing touch

--- main.js
+++ main.js
@@ -35,21 +35,42 @@ const mainTextAnimation = (objectValueContainer) => {
 }
 
 /**__START__TEXT-ANIMATION__REPLACEMENT__FUNCTION**/
-const replaceMain = (async (toAppendIn, toAddSkills, newElementID, toAddMainContent, secondElementID) => {
+const replaceMain = (async (toAppendIn, toAddSkills, newElementID, toAddMainContent, secondElementID, toAddTags) => {
     await mainTextAnimation(toAppendIn);
     // Create the new elements
     const newElement = document.createElement('span');
+    const tildNewElement = document.createElement('span');
+    const tag = document.createElement('span');
+    const lineBreak = document.createElement('br');
+    const dollarCommand = document.createElement('span');
     const secondnewElement = document.createElement('span');
-    // add some styles on the new element
+    // adding some styles on the new element
     newElement.className = 'mfc';
     newElement.id = newElementID;
     newElement.textContent = toAddSkills;
-    // add some styles on the second new element
+    // adding some styles on the tild new element
+    tildNewElement.className = 'mfc';
+    tildNewElement.style.fontSize = '1.4em'
+    tildNewElement.textContent = ' ~ '
+    tildNewElement.style.color = '#f1e423e5'
+    // adding some styles on the tag new element
+    tag.style.color = '#06a187e5';
+    tag.textContent = `( ${toAddTags} )`;
+    // adding some styles on the dollar($) sign new element
+    dollarCommand.textContent = '$ '
+    newElement.className = 'mfc';
+    dollarCommand.style.fontSize = '1.1em'
+    dollarCommand.style.color = '#ffffff'
+    // adding some styles on the second new element
     secondnewElement.textContent = toAddMainContent;
     secondnewElement.className = 'mfc';
     secondnewElement.id = secondElementID;
-    // Append the new element to nav1
+    // Appending the new element to nav1
     toAppendIn.appendChild(newElement);
+    toAppendIn.appendChild(tildNewElement);
+    toAppendIn.appendChild(tag);
+    toAppendIn.appendChild(lineBreak);
+    toAppendIn.appendChild(dollarCommand);
     toAppendIn.appendChild(secondnewElement);
     // Replace the nav1 element with the new element
 });
@@ -71,9 +92,9 @@ const interFacelessContent = ' Expert in developing API-driven solutions, automa
 const dataScienceContent = ' Proficient in data analysis and visualization, utilizing machine learning in and python libraries like Pandas and Scikit-learn to drive data-driven decision making'
 const academicsContent = ' Strong academic background with skills in critical thinking, reasearch and problem-solving, demonstrated through successful project completions and a commitment to continuous learning and grouwth'
 
-replaceMain(mainFirstChildContainer1, mfc_1, 'mfcc_1', programmingContent, 'mfccc_1');
-replaceMain(mainFirstChildContainer2, mfc_2, 'mfcc_2', developmentContent, 'mfccc_2');
-replaceMain(mainFirstChildContainer3, mfc_3, 'mfcc_3', interFacelessContent, 'mfccc_3');
-replaceMain(mainFirstChildContainer4, mfc_4, 'mfcc_4', dataScienceContent, 'mfccc_4');
-replaceMain(mainFirstChildContainer5, mfc_5, 'mfcc_5', academicsContent, 'mfccc_5');
+replaceMain(mainFirstChildContainer1, mfc_1, 'mfcc_1', programmingContent, 'mfccc_1', 'HABBIT');
+replaceMain(mainFirstChildContainer2, mfc_2, 'mfcc_2', developmentContent, 'mfccc_2', 'COOKING');
+replaceMain(mainFirstChildContainer3, mfc_3, 'mfcc_3', interFacelessContent, 'mfccc_3', 'DIVING');
+replaceMain(mainFirstChildContainer4, mfc_4, 'mfcc_4', dataScienceContent, 'mfccc_4', 'CRAZY-SCIENTIST');
+replaceMain(mainFirstChildContainer5, mfc_5, 'mfcc_5', academicsContent, 'mfccc_5', 'MENTORSHIP');
 /** END text animation function calls */
